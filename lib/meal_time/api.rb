3a5967932c2664429d1b5efb49fd5056ab732102
anikachristopher <anika.mchristoper@gmail.com>
2019-12-07 18:19:05 -0500

    change gemspec to include httparty dependency

--- lib/meal_time/api.rb
+++ lib/meal_time/api.rb
@@ -1,6 +1,8 @@
 class MealTime::API
 
-    BASE_URL= 'https://www.themealdb.com/api/json/v1/1/filter.php?'
+    BASE_URL= 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken'
+    BASE_URL= 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood'
+    BASE_URL= 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef'
     
     
     def self.query_meals_db(query)
