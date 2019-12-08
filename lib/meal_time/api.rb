2c2a5e6c9fcb881e3e57b657e53c9242223c752e
anikachristopher <anika.mchristoper@gmail.com>
2019-12-07 20:08:06 -0500

    install gem files

--- lib/meal_time/api.rb
+++ lib/meal_time/api.rb
@@ -1,10 +1,22 @@
 class MealTime::API
 
-    BASE_URL= 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken'
-    BASE_URL= 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood'
-    BASE_URL= 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef'
+    CHK_URL= 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken'
+    SEAF_URL= 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood'
+    BEEF_URL= 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef'
+    API_KEY = '1'
     
     
-    def self.query_meals_db(query)
-
+    
+    def self.chicken_meals_db(query)
+        response = HTTParty.get(CHK_URL)
     end
+
+    def seafood_meals_db(query)
+        response = HTTParty.get(SEAF_URL)
+    end
+
+    def beef_meals_db(query)
+        response = HTTParty.get(BEEF_URL)
+    end
+
+end
