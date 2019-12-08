2759ae9292b18237cbe54d9b5f7d590ab245eeb0
anikachristopher <anika.mchristoper@gmail.com>
2019-12-07 22:55:09 -0500

    introduce more text to the call method

--- lib/meal_time/cli.rb
+++ lib/meal_time/cli.rb
@@ -1,7 +1,9 @@
 class MealTime::CLI
     
     def call
+        puts ""
         puts "Welcome to the Meals Database!"
+        puts "There are 3 categories to choose from:"
         menu
         user_selection #more_info
         another_option
@@ -9,11 +11,24 @@ class MealTime::CLI
     end
 
     def menu 
-        puts "Which category of meals would you like to get more info on?"
+        puts "Which category of meals would you like to get more info on:?"
 
-        input = gets.strip #removing spaces fron the front and end
+        category = gets.strip #removing spaces fron the front and end
 
-        MealTime::API.query_meal_db(input)
+        MealTime::API.meal_db
+       
+
+        MealTime::Meals.all.each_with_index do |c, i|
+            puts "#{i + 1}".
+        end
+
+     def user_selection
+        input = gets.strip.downcase
+        if input = exit   
+        else 
+            puts "Sorry, this is an invalid option. Please make another choice!"
+            menu
+        end
     end 
 
 
