ae21554d0f7dc480dda771442aadf9267c20c524
anikachristopher <anika.mchristoper@gmail.com>
2019-12-07 23:53:10 -0500

    create new attributes in the categories file

--- lib/meal_time/categories.rb
+++ lib/meal_time/categories.rb
@@ -1,7 +1,25 @@
 class Meal_Time::Categories
 #this class will initialize with the category attributes
-#it will save all ibstances of itself in the @@all class
-#
+#it will save all instances of itself in the @@all class
+#need a mtehod to read the @@all class
+attr_accessor :idCategory, :strCategory, :strCategoryDescription
+
+@@all = []
+
+def initialize(args)
+    args.each do |k,v|
+        self.send(("#{k}="), v)
+    
+    @@all << self
+    end    
+
+end
+
+def self.all
+    @@all
+end
+
+
 end
 
 
