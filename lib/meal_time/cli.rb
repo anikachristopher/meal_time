5baa0bc6869999d1f64a47595830b84e61032367
anikachristopher <anika.mchristoper@gmail.com>
2019-12-08 22:30:38 -0500

    update error prompt

--- lib/meal_time/cli.rb
+++ lib/meal_time/cli.rb
@@ -21,7 +21,7 @@ class MealTime::CLI
             end
             category_selection
         else 
-            puts "Sorry...I don't understand that selection, please choose another number!".colorize(:red)  
+            puts "Sorry...I don't understand that selection, please make a valid choice!".colorize(:red)  
             search
         end  
     end    
@@ -55,7 +55,7 @@ class MealTime::CLI
             meal_info(meal)
             new_search
         else 
-            puts "Sorry...I don't understand that selection, please choose another number!".colorize(:red)
+            puts "Sorry...I don't understand that selection, please make a valid choice!".colorize(:red)
             category_selection
         end    
     end  
